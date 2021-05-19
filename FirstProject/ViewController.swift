0d0696d8d14a15888767ef530b5b2560331a8aa2
Vladislav Todorov <68444378+Fenominall@users.noreply.github.com>
2021-05-19 10:49:21 +0300

    Update ViewController.swift

--- FirstProject/ViewController.swift
+++ FirstProject/ViewController.swift
@@ -5,23 +5,21 @@
 //  Created by Fenominall on 18.05.2021.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
     
-    private lazy var loginContentView: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
+    private lazy var loginContentLabel: UILabel = {
+        let loginLabel = UILabel(frame: .zero)
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        return loginLabel
     }()
-    
-    private let myUsername: UITextField = UITextField(frame: CGRect(x: 25, y: 320, width: 320.00, height: 50.00));
-    private let myPassword: UITextField = UITextField(frame: CGRect(x: 25, y: 380, width: 320.00, height: 50.00));
     
    private let usernameTxtField: UITextField = {
        let textField = UITextField()
        textField.translatesAutoresizingMaskIntoConstraints = false
-       textField.backgroundColor = UIColor.white
+       textField.backgroundColor = .white
        textField.placeholder = "Username"
        textField.borderStyle = .roundedRect
        return textField
@@ -30,7 +28,7 @@ class ViewController: UIViewController {
    private let passwordTxtField: UITextField = {
        let textField = UITextField()
        textField.translatesAutoresizingMaskIntoConstraints = false
-       textField.backgroundColor = .red
+       textField.backgroundColor = .whote
        textField.placeholder = "Password"
        textField.borderStyle = .roundedRect
        return textField
@@ -51,43 +49,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        displayLoginField()
-        
         view.backgroundColor = .white
         
-        loginContentView.addSubview(usernameTxtField)
-        loginContentView.addSubview(passwordTxtField)
-        loginContentView.addSubview(btnLogin)
+        // loginContentView.addSubview(usernameTxtField)
+        // loginContentView.addSubview(passwordTxtField)
+        // loginContentView.addSubview(btnLogin)
 
-        view.addSubview(loginContentView)
+        view.addSubview(loginContentLabel)
 
         NSLayoutConstraint.activate([
-            loginContentView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            loginContentView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10),
-            loginContentView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginContentLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            loginContentLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10),
+            loginContentLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
 //            Button
-            btnLogin.topAnchor.constraint(equalTo: loginContentView.bottomAnchor, constant: 450),
-            btnLogin.leftAnchor.constraint(equalTo: loginContentView.leftAnchor, constant: 15),
-            btnLogin.rightAnchor.constraint(equalTo: loginContentView.rightAnchor, constant: -40),
-            btnLogin.heightAnchor.constraint(equalToConstant: 50)
+            // btnLogin.topAnchor.constraint(equalTo: loginContentView.bottomAnchor, constant: 450),
+            // btnLogin.leftAnchor.constraint(equalTo: loginContentView.leftAnchor, constant: 15),
+            // btnLogin.rightAnchor.constraint(equalTo: loginContentView.rightAnchor, constant: -40),
+            // btnLogin.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
     
-    func displayLoginField() {
-//        Username 
-        myUsername.placeholder = "Username"
-        myUsername.borderStyle = UITextField.BorderStyle.line
-        myUsername.backgroundColor = UIColor.white
-        myUsername.textColor = UIColor.gray
-        self.view.addSubview(myUsername)
-//        Password
-        myPassword.placeholder = "Password"
-        myPassword.borderStyle = UITextField.BorderStyle.line
-        myPassword.backgroundColor = UIColor.white
-        myPassword.textColor = UIColor.gray
-        myPassword.isSecureTextEntry = true
-        self.view.addSubview(myPassword)
-    }
     
 }
+
 

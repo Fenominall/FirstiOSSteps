9111a5aad0e50e09a97695a50a8fc94809f1be05
test <test>
2021-05-19 21:37:58 +0300

    added screen and text

--- FirstProject/ViewController.swift
+++ FirstProject/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private lazy var loginContentLabel: UILabel = {
-        let loginLabel = UILabel(frame: .zero)
-        loginLabel.translatesAutoresizingMaskIntoConstraints = false
-        return loginLabel
+    
+    private lazy var loginContentView:UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
+
+    
+    private lazy var logScreennText: UILabel = {
+        let screenText = UILabel()
+        screenText.translatesAutoresizingMaskIntoConstraints = false
+        screenText.text = "Welcome!"
+        screenText.font = screenText.font.withSize(40)
+        screenText.textColor = .white
+        return screenText
+    }()
+
+    
+    
     
    private let usernameTxtField: UITextField = {
        let textField = UITextField()
@@ -49,29 +63,61 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUpView()
         setUpConstraints()
+        setUpView()
+        
 
-        view.addSubview(loginContentLabel)
+        view.addSubview(loginContentView)
 
         NSLayoutConstraint.activate([
-            loginContentLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            loginContentLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10),
-            loginContentLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginContentView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            loginContentView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 10),
+            loginContentView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
     }
 
     func setUpConstraints() {
+        
+//        Screen message
+        view.addSubview(logScreennText)
+        
+        NSLayoutConstraint.activate([
+            logScreennText.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+            logScreennText.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 110),
+            logScreennText.heightAnchor.constraint(equalToConstant: 50)
+            
+        ])
+        
+//        Username View
+        view.addSubview(usernameTxtField)
+        
+        NSLayoutConstraint.activate([
+            usernameTxtField.topAnchor.constraint(equalTo: view.topAnchor, constant: 370),
+            usernameTxtField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
+            usernameTxtField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
+            usernameTxtField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+
+//        Password View
+        view.addSubview(passwordTxtField)
+        
+        NSLayoutConstraint.activate([
+            passwordTxtField.topAnchor.constraint(equalTo: view.topAnchor, constant: 425),
+            passwordTxtField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
+            passwordTxtField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
+            passwordTxtField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+//        Button View
         view.addSubview(btnLogin)
 
         NSLayoutConstraint.activate([
-            btnLogin.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -350),
+            btnLogin.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -300),
             btnLogin.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
             btnLogin.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
             btnLogin.heightAnchor.constraint(equalToConstant: 50)
         ])
         
-        view.addSubview(usernameTxtField)
     }
 
     func setUpView() {

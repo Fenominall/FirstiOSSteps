3c3a48d7b4b13203a0b5c838e23d6b972118050f
Vladislav Todorov <68444378+Fenominall@users.noreply.github.com>
2021-06-15 09:22:02 +0300

    Update Constants.swift

--- FirstProject/Constants.swift
+++ FirstProject/Constants.swift
@@ -1,12 +1,12 @@
 import UIKit
 
 struct Images {
-  static let pirate = UIImage(named: "pirate")
-  static let laugh = UIImage(named: "laugh")
-  static let glasses = UIImage(named: "glasses")
-  static let smile = UIImage(named: "smile")
-  static let neon = UIImage(named: "neon")
-  static let gradient = UIImage(named: "gradient")
-  static let gradient2 = UIImage(named: "gradient2")
-  static let user = UIImage(named: "user")
+  static let pirate = UIImage(named: "pirate")!
+  static let laugh = UIImage(named: "laugh")!
+  static let glasses = UIImage(named: "glasses")!
+  static let smile = UIImage(named: "smile")!
+  static let neon = UIImage(named: "neon")!
+  static let gradient = UIImage(named: "gradient")!
+  static let gradient2 = UIImage(named: "gradient2")!
+  static let user = UIImage(named: "user")!
 }

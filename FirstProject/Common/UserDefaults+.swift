3ccfce19664320b85b84590241cabf4476013628
test <test>
2021-07-03 16:26:47 +0300

    UserDefaults changed

--- FirstProject/Common/UserDefaults+.swift
+++ FirstProject/Common/UserDefaults+.swift
@@ -38,9 +38,9 @@ extension UserDefaults: ObjectSavable {
 }
 
 enum ObjectSavableError: String, LocalizedError {
-    case unableToEncode = "Unable to encode object into data"
-    case noValue = "No data object found for the given key"
-    case unableToDecode = "Unable to decode object into given type"
+    case unableToEncode = "Unable to encode object into data."
+    case noValue = "No data object found for the given key."
+    case unableToDecode = "Unable to decode object into given type."
 
     var errorDescription: String? {
         rawValue

194f3f673ba13618176e72a17950ecabf782b5e4
Vladislav Todorov <68444378+Fenominall@users.noreply.github.com>
2021-08-26 16:55:02 +0300

    Update MainCoordinator.swift

--- FirstProject/MVVM/Controllers/MainCoordinator.swift
+++ FirstProject/MVVM/Controllers/MainCoordinator.swift
@@ -6,12 +6,17 @@ class MainCoordinator: Coordinator {
     var navigationController: UINavigationController?
 
     func eventOccured(with type: Event) {
-
+        switch type {
+            case .buttonTapped:
+                let homeVC: UIViewController & Coordinating = HomeScreenViewController()
+                homeVC.coordinator = self
+                navigationController?.pushViewController(homeVC, animated: true)
+        }
     }
 
     func start() {
         var loginViewController: UIViewController & Coordinating = LoginScreenViewController()
         loginViewController.coordinator = self
-        navigationController.setViewControllers([vc], animated: true)
+        navigationController.setViewControllers([vc], animated: false)
     }
 }

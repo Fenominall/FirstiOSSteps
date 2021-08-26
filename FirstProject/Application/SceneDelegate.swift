d6b3cd1ed4ab7b366b3a88048ee9da59ea784a41
Vladislav Todorov <68444378+Fenominall@users.noreply.github.com>
2021-08-26 16:12:45 +0300

    Update SceneDelegate.swift

--- FirstProject/Application/SceneDelegate.swift
+++ FirstProject/Application/SceneDelegate.swift
@@ -16,22 +16,28 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let navVC = UINavigationController()
+        
+        let coordinator = MainCoordinator()
+        coordinator.navigationController = navVC
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
-        let navVC = UINavigationController(rootViewController: LoginScreenViewController())
         window?.rootViewController = navVC
+        self.window = window
         
-//         let layout = UICollectionViewFlowLayout()
-//         layout.scrollDirection = .horizontal
-//         let swipingController = SwipingController(collectionViewLayout: layout)
-//         window?.rootViewController = swipingController
+        coordinator.start()
         
+//         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+//         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+//         window?.windowScene = windowScene
+//         window?.makeKeyAndVisible()
+//         let navVC = UINavigationController(rootViewController: LoginScreenViewController())
+//         window?.rootViewController = navVC     
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
